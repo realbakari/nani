13654257821d63cbfd335bb5b3f58424a520f2b1
Bakari Mustafa <contact@bakarimustafa.com>
2020-06-04 14:24:29 +1000

    Update AboutAlert.js

--- src/components/Alerts/AboutAlert.js
+++ src/components/Alerts/AboutAlert.js
@@ -9,16 +9,8 @@ class AboutAlert extends Component {
     const { dispatch, visible } = this.props
     return (
       <Alert color='info' isOpen={visible} toggle={() => dispatch(hideAbout())}>
-        <!-- <h5 className='alert-heading'>What is nani?</h5> -->
-        <!-- <p className='mb-0'>
-          nani is an unofficial client for Crunchyroll. It provides an interface for Crunchyroll that is fast and easy to use, all while providing handy features, such as AniList updating.
-
-          <br /><br />
-
-          It is 100%{' '}
-          <a href='https://github.com/destruc7i0n/nani' target='_blank' rel='noopener noreferrer'>open source</a>
-          , and communicates directly with Crunchyroll - no data is ever stored.
-        </p> -->
+        <h5 className='alert-heading'></h5> 
+        
       </Alert>
     )
   }
